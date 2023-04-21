eb1f8ad6a80df1fdcb69d63babd3cd865d89ae7a
Bravishka Skytano <yolson@liberty.edu>
2023-04-21 18:24:49 -0400

    Add initial nvim config

---
+++ .config/nvim/init.lua
@@ -0,0 +1,13 @@
+--[[ init.lua ]]
+
+-- LEADER
+-- These keybindings need to be defined before the first /
+-- is called; otherwise, it will default to "\"
+vim.g.mapleader = "<space>"
+vim.g.localleader = "\\"
+
+-- IMPORTS
+require('vars')      -- Variables
+require('opts')      -- Options
+require('keys')      -- Keymaps
+-- require('plug')      -- Plugins
